31cbef541b1c7c0543aa784cad56adb754e03411
Panuawat <116491852+Panuawat@users.noreply.github.com>
2024-05-25 15:20:43 +0700

    1- notification routes and controllers added

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,12 @@ import express from 'express';
 import { v2 as cloudinary } from 'cloudinary'
 import authRoutes from './routes/auth.route.js'
 import userRoutes from './routes/user.route.js'
+import postRoutes from './routes/post.route.js'
+import notificationRoutes from './routes/notification.route.js'
 import dotenv from 'dotenv'
 import connectMongoDB from './db/connectMongoDB.js';
 import cookieParser from 'cookie-parser';
-import postRoutes from './routes/post.route.js'
+
 
 const app = express();
 const PORT = process.env.PORT || 5000
@@ -24,7 +26,7 @@ app.use(express.urlencoded({extended:true}))
 app.use('/api/auth',authRoutes)
 app.use('/api/users',userRoutes)
 app.use('/api/posts',postRoutes)
-
+app.use('/api/notifications',notificationRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
